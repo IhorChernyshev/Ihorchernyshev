af06da159e17b881a346dfc9b38e6577839790a1
garik <Ihorchernyshev1@gmail.com>
2024-05-26 13:51:50 +0100

    dz

---
+++ dz2.2.py
@@ -0,0 +1,7 @@
+qwe = int(input('Будь-яке 5-х значне число'))
+zxc1 = qwe // 10000
+zxc2 = (qwe // 1000) %10
+zxc3 = (qwe // 100) %10
+zxc4 = (qwe // 10) %10
+zxc5 = qwe % 10
+print(zxc5,zxc4,zxc3,zxc2,zxc1)
