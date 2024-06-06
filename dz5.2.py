c7724f52f5a44092200f2af0a70bda1a1060a2b5
garik <Ihorchernyshev1@gmail.com>
2024-06-06 13:11:45 +0100

    dz

---
+++ dz5.2.py
@@ -0,0 +1,22 @@
+while True:
+        first_number = int(input('write first number'))
+        second_number = int(input('write second number'))
+        third_operation = input("write operation")
+        if third_operation == '+':
+            print(first_number + second_number)
+        elif third_operation == '-':
+            print(first_number - second_number)
+        elif third_operation == '/' and second_number != 0:
+            print(first_number / second_number)
+        elif third_operation == '*':
+            print(first_number * second_number)
+        else:
+            print('Dividing by zero is not allowed')
+        cont = input("Would you like to continue? (y/n): ").lower()
+        if cont != 'y':
+            print("The calculator is completed.")
+            break
+
+
+
+
