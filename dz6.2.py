20a3f7dc9ef72d70d789c47a09b88166b45d3718
garik <Ihorchernyshev1@gmail.com>
2024-06-13 14:53:25 +0100

    dz

--- dz6.2.py
+++ dz6.2.py
@@ -1,13 +1,18 @@
-input_seconds = int(input("Enter the number of seconds: "))
-if input_seconds < 0 or input_seconds >= 8640000:
-    print("Invalid value. Please enter a number greater than or equal to 0 and less than 8640000.")
-
+seconds_input = int(input("Введіть кількість секунд: "))
+if seconds_input < 0 or seconds_input >= 8640000:
+    print("Невірне значення. Введіть число більше або дорівнює 0 і менше ніж 8640000.")
 else:
-    days = input_seconds // (24 * 60 * 60)
-    hours = (input_seconds % (24 * 60 * 60)) // (60 * 60)
-    minutes = (input_seconds % (60 * 60)) // 60
-    seconds = input_seconds % 60
+    days, remainder = divmod(seconds_input, 24 * 60 * 60)
+    hours, remainder = divmod(remainder, 60 * 60)
+    minutes, seconds = divmod(remainder, 60)
+    if hours == 24:
+        days += 1
+        hours = 0
+    if 10 <= days % 100 <= 20:
+        days_word = 'днів'
+    else:
+        days_word = 'день' if days % 10 == 1 else 'дні' if 2 <= days % 10 <= 4 else 'днів'
 
-    time_str = f"{int(days)} {'день' if days == 1 else 'дні' if 2 <= days <= 4 else 'днів'}:{str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
+    x = f"{days} {days_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
 
-    print(time_str)
+    print(x)
