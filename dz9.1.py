2fa8e5022910af5048a4c17d706c39fec828596d
garik <Ihorchernyshev1@gmail.com>
2024-06-25 01:45:04 +0100

    dz

--- dz9.1.py
+++ dz9.1.py
@@ -5,6 +5,8 @@ def popular_words(text, words):
         count = words_list.count(word.lower())
         word_count[word] = count
     return word_count
+
+
 input_text = '''When I was One I had just begun When I was Two I was nearly new '''
 input_words = ['i', 'was', 'three', 'near']
 result = popular_words(input_text, input_words)
